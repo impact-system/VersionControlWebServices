9f7040264b14f4d487bb983e333882e650065d4b
impact-system <laxman.sangade@imapctsystem.com>
2020-07-29 16:23:13 +0530

    adding commit

---
+++ WebService.asmx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Services;
+
+namespace WebService
+{
+    /// <summary>
+    /// Summary description for WebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+
+    public class WebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public double Square(double x)
+        {
+            return x * x * x;
+        }
+
+        [WebMethod]
+        public int addition(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+    }
+}
